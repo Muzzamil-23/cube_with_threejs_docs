1476be17de832b6d63e042eba7efa14e59abdecf
Muzzammil Siddiqui <muzzamil.siddiqui20@gmail.com>
2026-03-24 23:45:19 +0500

    threejs cube

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Cube from './components/Cube'
+
+function App() {
+  return (
+    <>
+    <div className='w-full h-full overflow-hidden'>
+      <Cube/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TexturedCube.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TexturedCube = () => {
+  return (
+    <div>TexturedCube</div>
+  )
+}
+
+export default TexturedCube
